5e1cb2f1e8d5895f23a33e4fa891df710fbf9be3
Zer <livefatezz@gmail.com>
2023-01-20 19:38:43 +0800

    init

---
+++ mandatory/check_arg.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_arg.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chenlee <chenlee@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 19:19:47 by chenlee           #+#    #+#             */
+/*   Updated: 2023/01/20 19:30:49 by chenlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	error(int condition)
+{
+	if (condition == 1)
+	{
+		printf("Usage: ./philo <num_of_philo> <time_to_die> <time_to_eat>"
+			" <time_to_sleep> <(optional)num_of_times_each_philo_must_eat>\n");
+	}
+	else if (condition == 2)
+		printf("Error: Parameters must contain only numbers!\n");
+	else if (condition == 3)
+		printf("Error: Too many Philosophers!\n");
+	exit(1);
+}
+
+void	check_isnum(char *line)
+{
+	int	i;
+
+	i = -1;
+	while (line[++i] != 0)
+	{
+		if (line[i] > 57 || line[i] < 48)
+			error(2);
+	}
+}
+
+void	check_arguments(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc != 5 || argc != 6)
+		error(1);
+	else
+	{
+		while (++i < argc)
+			check_isnum(argv[i]);
+		if (ft_atoi(argv[1]) > 200)
+			error(3);
+	}
+}
---
+++ mandatory/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chenlee <chenlee@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 18:42:18 by chenlee           #+#    #+#             */
+/*   Updated: 2023/01/20 18:44:04 by chenlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+
+typedef	struct s_philo
+{
+	pthread_t	thread;
+}			t_philo;
